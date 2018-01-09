983428843b752e08bf400caf376d7f38f7386cb5
tarikhagustia <agustia.tarikh150@gmail.com>
2018-01-09 11:06:31 +0700

    adding scss

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -6,21 +6,6 @@ import Works from '../sections/Works.js';
 import Others from '../sections/Others.js';
 import Footer from '../sections/Footer.js';
 
-// <link rel="stylesheet" href="css/linearicons.css">
-// <link rel="stylesheet" href="css/owl.carousel.css">
-// <link rel="stylesheet" href="css/font-awesome.min.css">
-// <link rel="stylesheet" href="css/nice-select.css">
-// <link rel="stylesheet" href="css/magnific-popup.css">
-// <link rel="stylesheet" href="css/bootstrap.css">
-// <link rel="stylesheet" href="css/main.css">
-
-import './../../css/bootstrap.css';
-import './../../css/linearicons.css';
-// import './../../css/owl.carousel.css';
-// import './../../css/font-awesome.css';
-import './../../css/nice-select.css';
-import './../../css/magnific-popup.css';
-import './../../css/main.css';
 
 class Home extends Component {
    render() {
@@ -30,7 +15,7 @@ class Home extends Component {
             <Header/>
             <Featured/>
             <Services/>
-          </div>
+          </div> 
 
           <div className="main-wrapper">
             <Works/>
